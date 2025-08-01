37491fcd575258460c429a3bc074e77a8e85f136
Abhijith V <abhijithababhijith@gmail.com>
2025-08-01 18:21:01 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/abhisawesome/',
+  base: '/terminal-portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
