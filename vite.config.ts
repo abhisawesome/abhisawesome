290bd6556ba038083f706d55be80559abed522a7
Abhijith V <abhijithababhijith@gmail.com>
2026-04-08 15:33:35 +0530

    deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/abhisawesome/',
+  base: process.env.VITE_BASE_PATH || '/abhisawesome/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
